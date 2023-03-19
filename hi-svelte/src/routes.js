772e32ab08ff8a4f01b21204d914ecc8cf98a142
김재형 <cogito1016@naver.com>
2023-03-19 16:53:54 +0900

    feat: 라우팅 구조

--- hi-svelte/src/routes.js
+++ hi-svelte/src/routes.js
@@ -1,12 +1,12 @@
 import Home from "./routes/Home.svelte";
-import Lorem from "./routes/Lorem.svelte";
 import NotFound from "./routes/NotFound.svelte";
-import Exam from "./routes/Exam.svelte";
+import Info from "./routes/Info.svelte";
+import ImageBoard from "./routes/Image-Board.svelte";
 
 export default {
   "/": Home,
-  "/lorem/:repeat": Lorem,
-  "/exam": Exam,
+  "/info": Info,
+  "/image-board": ImageBoard,
   // The catch-all route must always be last
   "*": NotFound,
 };
---
+++ hi-svelte/src/routes/Image-Board.svelte
@@ -0,0 +1,9 @@
+<h1>Image Board</h1>
+<h2>고양이 이미지 공유 게시판</h2>
+
+<p>
+	<a href="/">Home</a>
+    <a href="#/info">INFO</a>
+    <a href="#/image-board">IMAGE BOARD</a>
+</p>
+
