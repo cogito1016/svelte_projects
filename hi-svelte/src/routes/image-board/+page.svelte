cc9757ef85fae4b23f86bea0a2f236ff3e593fb2
김재형 <cogito1016@naver.com>
2023-03-20 00:03:16 +0900

    feat: 이미지게시판 목록 클릭요소로

--- hi-svelte/src/routes/image-board/+page.svelte
+++ hi-svelte/src/routes/image-board/+page.svelte
@@ -2,12 +2,13 @@
 <h1>Image Board</h1>
 <h2>고양이 이미지 공유 게시판</h2>
 
-<div class="card-display">
-    <h3>고양이 제목</h3>
-    <img alt="No-Imges" width="300px" height="300px" src="./images/cat.jpeg"/>
-    <h6>고양이 내용</h6>
-</div>
-
+<a href="javascript:void(0)">
+    <div class="card-display">
+        <h3>고양이 제목</h3>
+        <img alt="No-Imges" width="300px" height="300px" src="./images/cat.jpeg"/>
+        <h6>고양이 내용</h6>
+    </div>
+</a>
 <script>
 	import Nav from '../nav.svelte';
 </script>
