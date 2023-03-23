f46a994fe87dc88c5d502c8d827f6bf439e2dfb5
김재형 <cogito1016@naver.com>
2023-03-24 00:40:58 +0900

    style: Keypad UI styling

--- basic-svelte/src/routes/keypad/+page.svelte
+++ basic-svelte/src/routes/keypad/+page.svelte
@@ -1,12 +1,23 @@
 <script>
 // @ts-nocheck
-
     import Keypad from "./keypad.svelte";
+
 </script>
 
-<div>
+<div id="keypad">
     <h1>Key Pad</h1>
-    <Keypad id="keypadContainer" />
+    <input id="keypadResultInp" placeholder="Click the keypad element"/>
+    <Keypad />
 </div>
 
+<style>
+    #keypad{
+        text-align: center;
+    }
+    #keypadResultInp{
+        width:200px;
+        height:50px;
+        margin : 20px 0;
+    }
+</style>
 
