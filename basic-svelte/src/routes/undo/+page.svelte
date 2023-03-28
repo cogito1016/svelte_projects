bc79ceed77540040d9e87dd02e19dae7e71382b4
김재형 <cogito1016@naver.com>
2023-03-28 15:57:15 +0900

    feat: undo 테스트환경 구성

---
+++ basic-svelte/src/routes/undo/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    let list : number[] = [0];
+    let updateTargetNumber = 0;
+    let updateNumber = 0;
+
+    const add = (val : number) => {
+        list = [...list, val];
+    }
+
+    const changeNumber = () => {
+        const result = list.map((item)=>{
+            if(item===updateTargetNumber){
+                return updateNumber;
+            }
+            return item;
+        });
+        list = result;
+    }
+
+</script>
+
+<div>
+    <h1>
+        {list}
+    </h1>
+    <button on:click={()=>add(1)}>1</button>
+    <button on:click={()=>add(2)}>2</button>
+    <button on:click={()=>add(3)}>3</button>
+
+    <hr/>
+    <div>
+        <p>{updateTargetNumber}를 {updateNumber}로 변경합니다.</p>
+        <input bind:value={updateTargetNumber} />
+        <input bind:value={updateNumber} />
+        <button on:click={changeNumber}>변경</button>
+    </div>
+
+    <hr/>
+    <div>
+        <button>Undo</button>
+    </div>
+</div>
+
+<style>
+    h1{
+        background-color: white;
+        color: gray;
+    }
+</style>
