9fa57aab9e613f0a32b9c875fa0c93861f0461b4
김재형 <cogito1016@naver.com>
2023-03-30 23:41:28 +0900

    feat: svelte update & set

--- basic-svelte/src/routes/store/+page.svelte
+++ basic-svelte/src/routes/store/+page.svelte
@@ -4,9 +4,23 @@
 
     count.subscribe((value)=>countValue=value);
 
+    function add(){
+        count.update(n=>n+1)
+    }
+
+    function sub(){
+        count.update(n=>n-1);
+    }
+
+    function reset(){
+        count.set(0);
+    }
 </script>
 
 
 <div>
     this count is {countValue};
+    <button on:click={add}>증가</button>
+    <button on:click={sub}>감소</button>
+    <button on:click={reset}>초기화</button>
 </div>
