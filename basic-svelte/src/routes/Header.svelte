65fe0c0d8335f39851a97320a988cb0f7513b51a
김재형 <cogito1016@naver.com>
2023-03-30 23:39:30 +0900

    feat: svelte store subscribe

--- basic-svelte/src/routes/Header.svelte
+++ basic-svelte/src/routes/Header.svelte
@@ -31,6 +31,9 @@
 			<li aria-current={$page.url.pathname === '/undo' ? 'page' : undefined}>
 				<a href="/undo">Undo</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/store' ? 'page' : undefined}>
+				<a href="/store">Store</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
---
+++ basic-svelte/src/routes/store/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { count } from "./store";
+    let countValue : number;
+
+    count.subscribe((value)=>countValue=value);
+
+</script>
+
+
+<div>
+    this count is {countValue};
+</div>
