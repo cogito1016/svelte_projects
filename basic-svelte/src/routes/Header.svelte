de2ec752ed370ebcb3c1f4a736c136e313830e4e
김재형 <cogito1016@naver.com>
2023-04-03 00:53:21 +0900

    init: coffee-shop

--- basic-svelte/src/routes/Header.svelte
+++ basic-svelte/src/routes/Header.svelte
@@ -34,6 +34,9 @@
 			<li aria-current={$page.url.pathname === '/store' ? 'page' : undefined}>
 				<a href="/store">Store</a>
 			</li>
+			<li aria-current={$page.url.pathname === '/coffee-shop' ? 'page' : undefined}>
+				<a href="/coffee-shop">coffee-shop</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
